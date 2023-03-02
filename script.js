8bca5566f0db52cbcac7a9cab8b1e5687954ef49
ResoDevansh <sachandevansh2000@gmail.com>
2023-03-02 23:46:36 +0530

    about to finish desktop version

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ for (let i = 0; i < n; i++) {
   cardElement.innerHTML = peopleTestimonials[i];
   cardElement.style.cssText += `color: hsl(227, 12%, 61%); display: inline-block;margin-right: 5em;`;
   if (i == n - 1) {
-      cardElement.style.cssText += "display: none;";
+    cardElement.style.cssText += "display: none;";
     //   cardElement.classList.add('special');
   }
   if (i == 0) {
@@ -23,18 +23,77 @@ for (let i = 0; i < n; i++) {
   if (i == n - 2) {
     cardElement.style.cssText += "margin-right: -5em";
   }
-    cardElement.classList.add(`card-items-${i}`);
+  cardElement.classList.add(`card-items-${i}`);
   card.appendChild(cardElement);
 }
-document.getElementById('next').onclick = () => {
-    card.lastElementChild.style.cssText += 'display: inline-block;position: relative;top: -27.5vh;left: 35vw;';
-    for (let i = 0; i < n-1; i++){
-        document.getElementsByClassName(`card-items-${i}`)[0].style.cssText+='transition: 1s all ease-in-out;position:relative;left: -32vw;'
+document.getElementById("next").onclick = () => {
+  card.lastElementChild.style.cssText +=
+    "display: inline-block;position: relative;top: -27.5vh;left: 35vw;";
+  for (let i = 0; i < n - 1; i++) {
+    document.getElementsByClassName(`card-items-${i}`)[0].style.cssText +=
+      "transition: 1s all ease-in-out;position:relative;left: -32vw;";
+  }
+};
+document.getElementById("prev").onclick = () => {
+  card.lastElementChild.style.cssText += "display: none;";
+  for (let i = 0; i < n - 1; i++) {
+    document.getElementsByClassName(`card-items-${i}`)[0].style.cssText +=
+      "transition: 1s all ease-in-out;position:relative;left:0;";
+  }
+};
+let input = document.getElementById("footer-fourth-input");
+let modal = document.getElementById('modal');
+let copyright = document.getElementById('copy');
+input.onfocus = () => {
+  // this.style.border = '1px solid hsl(12, 88%, 59%)';
+  // document.getElementById()
+  // console.log('g');
+  input.style.border = "2px solid hsl(12, 88%, 59%)";
+};
+const inputHandler = (e) => {
+  e.preventDefault();
+  let isValid = true;
+  let email = e.target.value,
+    n = email.length,
+    period,
+    atr=0,per=0;
+  for (let i = n - 1; i >= 0; i--) {
+    if (email[i] === ".") {
+      period = i;
+      break;
     }
-}
-document.getElementById('prev').onclick = () => {
-    card.lastElementChild.style.cssText += 'display: none;';
-    for (let i = 0; i < n-1; i++){
-        document.getElementsByClassName(`card-items-${i}`)[0].style.cssText+='transition: 1s all ease-in-out;position:relative;left:0;'
+  }
+  for (let i = 0; i < n; i++){
+    if (email[i] === '@') {
+      indexAtr = i;
+      break;
     }
-}
+  }
+  for (let i = 0; i < n; i++){
+    if (email[i] === '@') atr++; 
+    if (email[i] === '.') per++;
+    if (!(email[i] === '.' || email[i] === '_' || (email[i] === '@' || email[i] === '-') || (email[i] >= 'a' && email[i] <= 'z') || (email[i] >= 'A' && email[i] <= 'Z') || (email[i] <= '9' && email[i] >= '0')))
+      isValid = false;
+  }
+  if (indexAtr <= 0) {
+    isValid = false;
+  }
+  if (atr + per === 0) {
+    isValid = false;
+  }
+  if (atr > 1) {
+    isValid = false;
+  }
+  if (n - period-1 <2) {
+    isValid = false;
+  }
+  if (!isValid) {
+    modal.style.display = 'block';
+    // copyright.style.position = 'absolute';  
+  }
+  else modal.style.display = 'none';
+  console.log(modal.style.display);
+};
+input.addEventListener("change", inputHandler);
+
+
